a11a8346aff5286ae17f7d541ee83919f3cf1fb3
Chandre Gowda <chandregowda@gmail.com>
2018-06-09 18:42:15 +0530

    Added more Utilities function to exceute 1. Commands in end servers 2. Ping servers 3. Loop through all the instances
    API: searchParamKeyInAllNodeInstances added
    It will search node server cobranded values in params.js

--- server/scanner/scanner.server.controller.js
+++ server/scanner/scanner.server.controller.js
@@ -131,9 +131,10 @@ function getApiForManyInstance(filters) {
         trackingId,
       };
 
-      console.log(
-        `[${requestCount}]: Sending API Scan request for ${environments} ${datacenters} ${ip} ${instance} ${component} ${deploymentMethod}`
-      );
+      // console.log(
+      //   `[${requestCount}]: Sending API Scan request for ${environments} ${datacenters} ${ip} ${instance} ${component} ${deploymentMethod}`
+      // );
+
       getApiForInstance(options).then(result => {
         responseCount++;
         // console.log(`[${trackingId}] : API SCAN Result :`, result);
@@ -237,9 +238,10 @@ function getComponentExceptionSummary(filters) {
         deploymentMethod
       } = process;
 
-      console.log(
-        `[${requestCount}]: Sending Log Scan request for ${environments[0]} ${datacenters[0]} ${ip} ${instance} ${component} ${deploymentMethod}`
-      );
+      // console.log(
+      //   `[${requestCount}]: Sending Log Scan request for ${environments[0]} ${datacenters[0]} ${ip} ${instance} ${component} ${deploymentMethod}`
+      // );
+
       getSingleInstanceExceptionSummary({
           mailTo,
           searchDate,
@@ -305,7 +307,7 @@ Scanner.getLogSummary = function (req, res) {
       let saveOptions = {
         accountName,
         summary: result,
-        excelFileName: logFolder + excelFileName,
+        excelFileName: path.resolve(logFolder, excelFileName),
         filters
       };
       if (result && result.length) {
@@ -316,10 +318,10 @@ Scanner.getLogSummary = function (req, res) {
             filename: excelFileName
           })
           .then((outputExcelFile) => {
-            console.log('Log Summary Excel File "', outputExcelFile, '" written');
+            // console.log('Log Summary Excel File "', outputExcelFile, '" written');
             saveExceptionSummary(saveOptions)
               .then((d) => {
-                console.log('Successfully saved Log Summary into DB');
+                // console.log('Successfully saved Log Summary into DB');
                 return res.send(saveOptions);
               })
               .catch((e) => {
@@ -338,11 +340,175 @@ Scanner.getLogSummary = function (req, res) {
       console.error('Error scanning Logs: ', e);
       return res.status(400).send(e);
     });
-}; // getLogSummary
+}; // API: getLogSummary
 
+
+
+function formatOutputSummary(ip, instance, details) {
+  let summary = [];
+  if (!details) return [];
+  let lines = details.split(/\n/);
+  // Loop through the output and create object
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    line = line.replace(/\s*=\s*/, ' ');
+    // line = line.replace(/exports.enable_iav_theme/, '');
+    let fields = line.split(' '); // count filename exception
+    if (fields.length >= 3) {
+      let cobrandId = '',
+        appId = '';
+      let count = parseInt(fields.shift());
+      let filename = fields.shift();
+      let keySplit = filename.split(':');
+      filename = keySplit[0].replace(':', '');
+      let key = keySplit[1];
+
+      let value = fields.shift();
+      value = value.replace(';', '');
+      value = value.replace('\r', '');
+
+      let fileSplit = filename.split(/\//);
+      let cobApp = fileSplit[5];
+      if (cobApp === 'default') {
+        continue;
+      }
+      if (cobApp) {
+        let cobAppSplit = cobApp.split(/-/);
+        cobrandId = cobAppSplit[0];
+        appId = cobAppSplit[1];
+      }
+      summary.push({
+        ip,
+        instance,
+        filename,
+        cobrandId,
+        appId,
+        key,
+        value
+      });
+    }
+  }
+  return summary;
+} // Function : formatOutputSummary
+
+function getFolderToSearch(o) {
+  let folderPath = "";
+  let logPath = `/opt/${o.deploymentMethod}`;
+  let logFolder = 'node' + o.instance + '/brands';
+
+  folderPath = path.resolve(logPath, logFolder);
+  // console.log(`Log Folder to search for ${o.component} -> ${o.instance} -> ${o.deploymentMethod} is ${folderPath}`)
+  return folderPath;
+} // Function : getFolderToSearch
+
+function getLookupFilePattern(o) {
+  return "-name params.js";
+} // Function : getLookupFilePattern
+
+function getSearchCommand(o) {
+  let searchString = o.searchString || 'nothingToSearch';
+
+  searchString = searchString.replace(/,/g, '|'); // replace , by | for grep regEx
+  searchString = searchString.replace(/\s*/g, ''); // remove all spaces
+
+  let logFolder = getFolderToSearch(o);
+  let logFilePattern = getLookupFilePattern(o);
+
+  let command = `find ${logFolder} ${logFilePattern} -exec grep -P "(${searchString}).*=.*" {} + 2>/dev/null | sort | uniq -c | awk "{\\\$2=\\\$2};1"`;
+  // console.log("getSearchCommand: ", command);
+  return command;
+} // Function : getSearchCommand
+
+function searchParamKeysInSingleInstance(options) {
+  // console.log("Inside searchParamKeysInSingleInstance", JSON.stringify(options, undefined, 2));
+
+  return new Promise((resolve, reject) => {
+    let command = getSearchCommand(options);
+
+    utils.executeCommand({
+      ip: options.ip,
+      command,
+      enclosure: 'SINGLE_QUOTES'
+    }).then((output) => {
+      // console.log(`Searched Param key for ${options.ip}->${options.instance}->${options.component} completed.`);
+      // console.log(output);
+      let formattedOutput = formatOutputSummary(options.ip, options.instance, output);
+      // console.log('FORMATTED Param Key OUTPUT', JSON.stringify(formattedOutput, undefined, 2));
+      resolve(formattedOutput);
+    }).catch(e => {
+      console.log("Failed to get param key", e);
+      reject(e);
+    });
+  });
+}; // Function: searchParamKeysInSingleInstance
+
+Scanner.searchParamKeyInAllNodeInstances = function (req, res) {
+  let filters = req.body;
+  // console.log("Inside searchParamKeyInAllNodeInstances", JSON.stringify(filters, undefined, 2));
+  let environment = filters.environments ? filters.environments[0] : null;
+  let datacenter = filters.datacenters ? filters.datacenters[0] : null;
+  let accountName = filters.mailTo ? filters.mailTo.replace(/@.*$/, '') : (req.decoded.sAMAccountName || 'unknown');
+
+  let apiName = "searchParamKeysInSingleInstance";
+  if (filters.searchString && filters.searchString.trim()) {
+    utils.loopThroughInstance(filters, apiName, searchParamKeysInSingleInstance)
+      .then(result => {
+        console.log(`Response for ${apiName} received with`, result.length);
+
+        let logDate = filters.searchDate || filters.logDate || moment().format('YYYY-MM-DD');
+        let trackingID = Date.now();
+        let logFolder = path.resolve(__dirname, '../../', CONFIG.LOGSUMMARY_FOLDER);
+        let excelFileName = `${environment}_${datacenter}_${filters.logType}_${logDate}_${accountName}_${trackingID}.xlsx`;
+        let saveOptions = {
+          accountName,
+          summary: result,
+          excelFileName: path.resolve(logFolder, excelFileName),
+          filters
+        };
+        if (result && result.length) {
+          utils
+            .writeToExcel({
+              jsonData: result,
+              folder: logFolder,
+              filename: excelFileName
+            })
+            .then((outputExcelFile) => {
+              console.log('Log Summary Excel File "', outputExcelFile, '" written');
+              saveExceptionSummary(saveOptions)
+                .then((d) => {
+                  console.log('Successfully saved Log Summary into DB');
+                  return res.send(saveOptions);
+                })
+                .catch((e) => {
+                  console.log(e);
+                  return res.send(e);
+                });
+            })
+            .catch((e) => {
+              console.log(e);
+              return res.send(e);
+            });
+        } else {
+          return res.send(saveOptions);
+        }
+
+
+
+        // res.send(result);
+      })
+      .catch(e => {
+        console.log(e);
+        res.status(500).send(e);
+      })
+  } else {
+    res.status(400).send({
+      message: 'Missing search Params'
+    });
+  }
+}; // API: searchParamKeyInAllNodeInstances
 
 // --searchParamKeyInAllNodeInstances --environments="production" --datacenters="sc9" --component="node" --paramKeys="enable_sense_theme|enable_iav_theme|enable_fl_theme|isCSSHostedEnabled|enable_unified_flow|ssoEnabled" -vvv --trackingId=1
-Scanner.searchParamKeyInAllNodeInstances = function (req, res) {
+Scanner.searchParamKeyInAllNodeInstancesOLD = function (req, res) {
   let apiName = "searchParamKeyInAllNodeInstances";
   let options = utils.getOptions(req, apiName);
   if (req.body.component) {
--- server/utils.js
+++ server/utils.js
@@ -1,18 +1,97 @@
 // Helper function that can be used commonly across controllers
-let fs = require("fs-extra");
-let path = require("path");
-let json2xls = require('json2xls');
-let exec = require('child_process').exec;
-// const moment = require('moment');
-let mkdirp = require('mkdirp');
+const fs = require("fs-extra");
+const path = require("path");
+const json2xls = require('json2xls');
+const exec = require('child_process').exec;
+const moment = require('moment');
+const mkdirp = require('mkdirp');
 
 const {
   CONFIG
 } = require('../config/config');
 
+function isValidIpAddress(ip) {
+  return /\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/.test(ip);
+} // Function End
+
+exports.pingServer = (ip) => {
+  let waitSeconds = 3, // ping timeout
+    cmd = `ping -c 3 -W ${waitSeconds} ${ip}`;
+
+  return new Promise((resolve, reject) => {
+    if (!isValidIpAddress(ip)) {
+      reject("Invalid IP Address");
+    }
+    // console.log(`Pinging Server ${ip}`);
+    exec(cmd, (error, stdout, stderr) => {
+      // console.log(`PING Completed for ${cmd}`);
+      if (stdout) {
+        if ((JSON.stringify(stdout) || '').indexOf('0 received') !== -1) {
+          reject(`${ip} Failed to PING`)
+        }
+        resolve(`${ip} PINGED successfully`);
+      } else if (stderr) {
+        console.log("STD ERR", JSON.stringify(stderr));
+        reject('STDERR');
+      } else if (error) {
+        console.log("ERROR: ", JSON.stringify(error));
+        reject('ERROR');
+      }
+    });
+  });
+}; // Function: pingServer
+
+/**
+ * @param {
+ *  ip -> Required -> IP Address to connect
+ *  command -> Required -> Command to execute in connected server
+ *  connectionTimeOut -> Optional -> default to 3000
+ *  enclosure -> Optional -> Should the command be enclosed in Single or Double quotes -> default to ", if single quote is required pass SINGLE_QUOTES as value
+ * } options 
+ * - return promise
+ */
+exports.executeCommand = (options) => {
+  let connectionTimeOut = options.connectionTimeOut || 300; // wait for 5 minutes max
+  let ConnectionAttempts = options.ConnectionAttempts || 2; // retry 2 times
+  // let tempFile = `/tmp/capiTEMP_${Date.now()}_${parseInt(Math.random()*10000000)}`;
+  let redirectToFile = (options.redirectOutputTo) ? ` > ${options.redirectOutputTo}` : '';
+
+  const SSH_OPTIONS = `ssh -o ConnectionAttempts=${ConnectionAttempts} -o ConnectTimeout=${connectionTimeOut} -o BatchMode=yes -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no `;
+
+  // NOTE: SSH commands sent to the end server is enclosed with DOUBLE QUOTES, so need to escape that character if used
+  let enclosure = options.enclosure && options.enclosure === 'SINGLE_QUOTES' ? "'" : '"'; // This should be double quotes by default or single qoute if overwritten
+  let command = `${SSH_OPTIONS} ${options.ip} ${enclosure} ls >/dev/null; ${options.command} ${enclosure}  2>/dev/null | awk ' { gsub (/Role changed to.*/, ""); }1 ' | awk ' NF > 0 '  ${redirectToFile}`;
+  if (redirectToFile) {
+    command = `${SSH_OPTIONS} ${options.ip} ${enclosure} ls >/dev/null; ${options.command} ${enclosure}  2>/dev/null |  tail -n+2 ${redirectToFile} `;
+  }
+
+  return new Promise((resolve, reject) => {
+    // "stdout maxbuffer exceeded" - to avoid this error set maxBugger to 5MB
+    exports.pingServer(options.ip).then(d => {
+      // console.log("SSH COMMAND :", command);
+      // 5120000000 bytes
+      exec(command, {
+        maxBuffer: 1024 * 5000000
+      }, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`SSH ERROR: Something went wrong while executing ${command}: ${error}`);
+          reject(error);
+        } else {
+          // stdout = stdout.replace(/Role changed to: Yodlee-restricted-role-logmntr/i, '');
+          // console.log(`Got SSH Response for ${options.ip}`);
+          stdout = stdout.replace(/\n$/, '');
+          resolve(stdout);
+        }
+        // console.log("STD SSH ERROR:", stderr);
+        // return reject(`SSH ERROR: ${command}`);
+      });
+    }).catch(e => {
+      reject(e);
+    });
+  });
+} // Function: executeCommand
 
 exports.createFolder = function (folderName) {
-  // Create temporary folder to save files
   // console.log("Creating folder:" + folderName);
   mkdirp(folderName, {
     mode: '0666' // Read and Write mode
@@ -28,7 +107,7 @@ exports.writeToExcel = function ({
   jsonData = {
     blank: 'nothing'
   },
-  folder = './public/downloads',
+  folder = './public/',
   filename = './temp.xlsx',
   shouldCompress = false
 }) {
@@ -120,3 +199,89 @@ exports.getOptions = (req, apiName) => {
 
   return options;
 } // Function : getOptions;
+
+/**
+ * 
+ * @param {mailTo, environments, datacenters, processInfo, <searchDate>} filters 
+ * processInfo is object of process details that contain:
+ *  ip, instance, component, deploymentMethod
+ * @param apiFunctionHandler - promise Function
+ */
+exports.loopThroughInstance = (filters, apiName, apiFunctionHandler) => {
+  console.log("Inside loopThroughInstance", JSON.stringify(filters, undefined, 2));
+  let currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+  let {
+    mailTo,
+    environments,
+    datacenters,
+    searchDate,
+    searchString,
+    processInfo
+  } = filters;
+
+  console.log(`${currentTime} ${mailTo}: ${apiName} - Looping through ${processInfo.length} process`);
+
+  return new Promise((resolve, reject) => {
+    let trackingId = Date.now();
+
+    let requestCount = 0;
+    let responseCount = 0;
+    let finalResult = [];
+
+    processInfo.forEach((process) => {
+      requestCount++;
+
+      let {
+        ip,
+        instance,
+        component,
+        deploymentMethod
+      } = process;
+
+      let options = {
+        mailTo,
+        environments,
+        datacenters,
+        ip,
+        instance,
+        component,
+        deploymentMethod,
+        logDate: searchDate,
+        searchString,
+        trackingId,
+      };
+
+      console.log(
+        `[${requestCount}]: Sending ${apiName} request for ${environments} ${datacenters} ${ip} ${instance} ${component} ${deploymentMethod}`
+      );
+
+      apiFunctionHandler(options).then(result => {
+        responseCount++;
+        // console.log(`[${trackingId}] : API SCAN Result :`, result);
+        try {
+          if (typeof result === 'string') {
+            finalResult = finalResult.concat(JSON.parse(result));
+          } else {
+            finalResult = finalResult.concat(result);
+          }
+        } catch (e) {
+          console.log(result);
+          console.log(`Failed to convert JSON response for ${apiName} request for ${environments} ${datacenters} ${ip} ${instance} ${component} ${deploymentMethod}`);
+          console.log(e);
+        }
+        checkForComplete();
+      }).catch(e => {
+        console.log(`Some error during ${apiName}`, e);
+        responseCount++;
+        checkForComplete();
+      });
+    }); // processInfo.forEach
+
+    function checkForComplete() {
+      // console.log(`[${responseCount}/${requestCount}]: [${trackingId}] : Total ${finalResult.length} API: ${apiName} `);
+      if (responseCount >= requestCount) {
+        resolve(finalResult);
+      }
+    }
+  });
+} // loopThroughInstance
